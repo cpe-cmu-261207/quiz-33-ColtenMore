98c1cd716d8c593cd037a2644be406b490b391a2
ColtenMore <aungpao321@gmail.com>
2022-09-15 14:27:14 +0700

    1

--- components/PostOwner.jsx
+++ components/PostOwner.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { comments } from "../libs/comments";
 
 export default function PostOwner(props) {
-  return <div></div>;
+  return (
+    <div className="vstack gap-3">
+    <div className="d-flex align-items-center gap-3">
+      <img
+        src="/profileImages/profile.jpg"
+        width="48"
+        height="48"
+        className="rounded-circle"
+        style={{ objectFit: "cover" }}
+      />
+      <span className="fw-semibold fs-5 text-white">
+        Thadthon Prechamanasart
+      </span>
+    </div>
+    <span className="text-white">
+      Quiz ยากจังเลยครับ ของ่าย ๆ กว่านี้ได้ไหม #261207
+    </span>
+    <div className="d-flex align-items-center gap-1">
+      <img src="/like.svg" width={20}></img>
+      <span style={{ color: "#B0B3B8" }}>727 คน</span>
+    </div>
+    <hr className="m-0 border" />
+  </div>
+    )
 }
--- components/Reply.jsx
+++ components/Reply.jsx
@@ -1,5 +1,48 @@
 import React from "react";
+import { comments } from "../libs/comments";
 
 export default function Reply(props) {
-  return <div></div>;
+  return (
+    props.prop.map((x)=>(
+      <div className="d-flex gap-2 my-2 ps-5">
+  <img
+    src={x.userImagePath}
+    width="48"
+    height="48"
+    className="rounded-circle"
+    style={{ objectFit: "cover" }}
+  />
+  <div
+    className="rounded rounded-3 p-2"
+    style={{ backgroundColor: "#3A3B3C" }}
+  >
+    <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+      {x.username}
+    </span>
+    <br />
+    <span style={{ color: "#E4E6EB" }}>{x.replyText}</span>
+  </div>
+</div>
+    ))
+
+//   <div className="d-flex gap-2 my-2 ps-5">
+//   <img
+//     src="/profileImages/puppy.jpg"
+//     width="48"
+//     height="48"
+//     className="rounded-circle"
+//     style={{ objectFit: "cover" }}
+//   />
+//   <div
+//     className="rounded rounded-3 p-2"
+//     style={{ backgroundColor: "#3A3B3C" }}
+//   >
+//     <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+//       หมาน้อย
+//     </span>
+//     <br />
+//     <span style={{ color: "#E4E6EB" }}>จริงค้าบบบบบบบบ</span>
+//   </div>
+// </div>
+);
 }
